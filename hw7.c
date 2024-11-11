8229a0455c9ddd0147e941153bd749fe6840fe12
aryonember55 <qkrtldn0p@naver.com>
2024-11-11 19:23:36 +0900

    Add Text File [hw7.c]

---
+++ hw7.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int arr1[6] = { 1, 2, 3, 4, 5, 6 }, arr2[6] = { 7, 8, 9, 10, 11, 12 };
+
+	printf("arr1 : ");
+	for (int i = 0; i < 6; i++)
+	{
+		printf("%d ", arr1[i]);
+	}
+	printf("\n");
+	printf("arr2 : ");
+		for (int i = 0; i < 6; i++)
+		{
+			printf("%d ", arr2[i]);
+		}
+	printf("\n\n");
+
+	int* p1 = &arr1[0];
+	int* p2 = &arr2[0];
+	int temp = 0;
+
+	for (int i = 0; i < 6; i++)
+	{
+		temp = *p1;
+		*p1 = *p2;
+		*p2 = temp;
+		p1++;
+		p2++;
+	}
+
+	printf("arr1 : ");
+	for (int i = 0; i < 6; i++)
+	{
+		printf("%d ", arr1[i]);
+	}
+	printf("\n");
+	printf("arr2 : ");
+	for (int i = 0; i < 6; i++)
+	{
+		printf("%d ", arr2[i]);
+	}
+
+	return 0;
+}
